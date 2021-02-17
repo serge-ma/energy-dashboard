1ce649c580e6e00ec739d61d66b99bf027949e42
sergej <sergej.malinoivski@gmail.com>
2021-02-17 15:06:09 +0100

    Update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 
 import plotly.express as px
 import pandas as pd
+import requests
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -14,18 +15,49 @@ server = app.server
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+# Accessing IMF database
+url = 'http://dataservices.imf.org/REST/SDMX_JSON.svc/'
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+commodities = {
+    'Crude, Brent':'POILBRE',
+    'Crude, WTI':'POILWTI',
+    'Crude, Dubai':'POILDUB',
+    'Natural gas, EU':'PNGASEU',
+    'Natural gas, US':'PNGASUS',
+    'Natural gas, Asia':'PNGASJP'}
+
+data = []
+for name, value in commodities.items():
+    data.append((requests.get(f'{url}CompactData/PCPS/M.W00.{value}.USD').json()['CompactData']['DataSet']['Series']))
+
+data_list = [[obs.get('@TIME_PERIOD'), obs.get('@OBS_VALUE')]
+             for obs in data[5]['Obs']]
+
+df = pd.DataFrame(data_list, columns=['Date', 'Price'])
+df['Date'] = pd.to_datetime(df['Date']) + pd.DateOffset(days=14)
+df = df.set_index('Date')
+df['Price'] = df['Price'].astype('float').round(2)
+df = df[df.index >= '2000-01-01']
+
+# df_orig = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+#
+# fig_orig = px.bar(df_orig, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig = px.line(df, x=df.index, y='Price', title='Natural Gas, Asia')
+fig.update_layout(xaxis_title='Year',
+                  xaxis_fixedrange=True,
+                  yaxis_title='Price (USD/MMBtu)',
+                  yaxis_fixedrange=True,
+                 template="seaborn")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash Update 2'),
+    html.H1(children='Energy Dashboard'),
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Source: IMF Primary Commodity Prices, Monthly data
     '''),
     dcc.Graph(
         id='example-graph',
